19828c86b5bb7dc22f4ec2f1cfcecff359aad785
fibboo <fibboo@gmail.com>
2022-01-30 12:36:19 +0300

    Fix created feilds. Start relation field

--- models.py
+++ models.py
@@ -1,13 +1,11 @@
-import datetime
+from sqlalchemy import func
 
 from settings import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # created = db.Column(
-    #     db.TIMESTAMP, default=datetime.datetime.now().timestamp()
-    # )
+    created = db.Column(db.DateTime, default=func.now())
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
@@ -18,19 +16,21 @@ class User(db.Model):
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # created = db.Column(
-    #     db.TIMESTAMP, default=datetime.datetime.now().timestamp()
-    # )
+    created = db.Column(db.DateTime, default=func.now())
     isbn = db.Column(db.Integer, unique=True, nullable=False)
     fio = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return self.fio
 
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # created = db.Column(
-    #     db.TIMESTAMP, default=datetime.datetime.now().timestamp()
-    # )
+    created = db.Column(db.DateTime, default=func.now())
     isbn = db.Column(db.Integer, unique=True, nullable=False)
     title = db.Column(db.String(120), nullable=False)
     review = db.Column(db.Text(), nullable=False)
-    author = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=True)
+    author = db.relationship('Author', backref=db.backref('books', lazy=True))
+
+    def __repr__(self):
+        return self.title
